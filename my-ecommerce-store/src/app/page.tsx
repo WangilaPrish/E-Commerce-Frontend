4c8cec420f050b8e5cd33bdd459fc1332489a2ef
WangilaPrish <nabwonyaprish@gmail.com>
2025-10-22 17:37:24 +0300

    fix: Remove unnecessary blank lines in page.tsx for cleaner code

--- my-ecommerce-store/src/app/page.tsx
+++ my-ecommerce-store/src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import CategoryGrid from "../components/CategoryGrid";
 import FeaturedProducts from "../components/FeaturedProducts";
-
 import Hero from "../components/Hero";
 import Newsletter from "../components/Newsletter";
 import products from "../lib/sample-products.json";
@@ -10,7 +9,6 @@ import products from "../lib/sample-products.json";
 export default function Home() {
     return (
         <div className="min-h-screen bg-background font-sans dark:bg-black">
-
             <main className="mx-auto max-w-6xl px-6 py-12">
                 <Hero />
                 <FeaturedProducts products={products as any} />
