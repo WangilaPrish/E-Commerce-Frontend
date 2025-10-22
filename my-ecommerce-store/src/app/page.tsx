345376c290d7377c23cef424d97ccf2962666056
WangilaPrish <nabwonyaprish@gmail.com>
2025-10-22 17:37:14 +0300

    fix: Remove unused Header component import from page.tsx

--- my-ecommerce-store/src/app/page.tsx
+++ my-ecommerce-store/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import CategoryGrid from "../components/CategoryGrid";
 import FeaturedProducts from "../components/FeaturedProducts";
-import Header from "../components/Header";
+
 import Hero from "../components/Hero";
 import Newsletter from "../components/Newsletter";
 import products from "../lib/sample-products.json";
@@ -10,7 +10,7 @@ import products from "../lib/sample-products.json";
 export default function Home() {
     return (
         <div className="min-h-screen bg-background font-sans dark:bg-black">
-            <Header />
+
             <main className="mx-auto max-w-6xl px-6 py-12">
                 <Hero />
                 <FeaturedProducts products={products as any} />
